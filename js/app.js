fd775e66bf3059095a2ad5510fa7c8634f97feac
Pedro Fragoso <167808367+pedrohtfragoso@users.noreply.github.com>
2024-05-15 13:26:09 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);    
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented')
+        botao.textContent = 'Alugar'
+        botao.classList.remove('dashboard__item__button--return')
+    } else { 
+        imagem.classList.add('dashboard__item__img--rented')
+        botao.textContent = 'Devolver'
+        botao.classList.add('dashboard__item__button--return')
+    }
+}
+
+
+
